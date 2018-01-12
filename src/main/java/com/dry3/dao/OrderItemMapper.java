70d0c9026049502867c44e693d69b48f153cc29c
dry3 <zyl@dry3.cn>
2018-01-12 08:59:51 +0800

    initalize mmall

---
+++ src/main/java/com/dry3/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.dry3.dao;
+
+import com.dry3.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
