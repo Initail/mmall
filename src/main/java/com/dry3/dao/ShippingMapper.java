ccc5f430e01988437152f2c18309615abd2e4a06
dry3 <zyl@dry3.cn>
2018-05-16 16:17:59 +0800

    aliyun deploy for password

--- src/main/java/com/dry3/dao/ShippingMapper.java
+++ src/main/java/com/dry3/dao/ShippingMapper.java
@@ -1,17 +1,1 @@
-package com.dry3.dao;
-
-import com.dry3.pojo.Shipping;
-
-public interface ShippingMapper {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(Shipping record);
-
-    int insertSelective(Shipping record);
-
-    Shipping selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(Shipping record);
-
-    int updateByPrimaryKey(Shipping record);
-}
+package com.dry3.dao;import com.dry3.pojo.Shipping;import org.apache.ibatis.annotations.Param;import java.util.List;public interface ShippingMapper {    int deleteByPrimaryKey(Integer id);    int insert(Shipping record);    int insertSelective(Shipping record);    Shipping selectByPrimaryKey(Integer id);    int updateByPrimaryKeySelective(Shipping record);    int updateByPrimaryKey(Shipping record);    int deleteByShippingIdUserId(@Param("userId")Integer userId, @Param("shippingId") Integer shippingId);    int updateByShipping(Shipping record);    Shipping selectByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId") Integer shippingId);    List<Shipping> selectByUserId(@Param("userId")Integer userId);}
